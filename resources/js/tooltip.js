704108491765033a70054e664c6107e4d784f04d
rrrhhhhhhhhh <rob.halverson@gmail.com>
2021-09-09 08:36:46 -0500

    Update tooltip.js

--- resources/js/tooltip.js
+++ resources/js/tooltip.js
@@ -1,46 +1,56 @@
 
+
 function initTooltip() {
-	const tooltips = Array.from(document.querySelectorAll('[data-tooltip-container]'));
+    const tooltips = Array.from(document.querySelectorAll('[data-tooltip-container]'));
+    let mouseIn = true;
 
-	tooltips.map(tooltip => {
-		tooltip.addEventListener('mouseover', handleMouseOver);
-	})
+    tooltips.map(tooltip => {
+        tooltip.addEventListener('mouseenter', handleMouseEnter);
+    })
 
-	function handleMouseOver() {
-		const tooltipbox = createTooltipBox(this);
+    function handleMouseEnter() {
+        console.log("Mouse enter");
+        mouseIn = true;
+        const tooltipbox = createTooltipBox(this);
 
-		handleMouseMove.tooltipbox = tooltipbox;
-		this.addEventListener('mousemove', handleMouseMove);
+        handleMouseMove.tooltipbox = tooltipbox;
+        this.addEventListener('mousemove', handleMouseMove);
 
-		handleMouseLeave.tooltipbox = tooltipbox;
-		handleMouseLeave.element = this;
-		this.addEventListener('mouseleave', handleMouseLeave);
-	}
+        handleMouseLeave.tooltipbox = tooltipbox;
+        handleMouseLeave.element = this;
+        this.addEventListener('mouseleave', handleMouseLeave);
+    }
 
-	const handleMouseLeave = {
-		handleEvent() {
-			this.tooltipbox.remove();
-			this.element.removeEventListener('mousemove', handleMouseMove);
-			this.element.removeEventListener('mouseleave', handleMouseLeave);
-		}
-	}
+    const handleMouseLeave = {
+        handleEvent() {
+            mouseIn = false;
+            console.log("Mouse leave");
+            this.tooltipbox.remove();
+            this.element.removeEventListener('mousemove', handleMouseMove);
+            this.element.removeEventListener('mouseleave', handleMouseLeave);
+        }
+    }
 
-	const handleMouseMove = {
-		handleEvent(e) {
-			this.tooltipbox.style.top = e.clientY + 25 + 'px';
-			this.tooltipbox.style.left = e.clientX + 13 +'px';
-		}
-	}
+    const handleMouseMove = {
+        handleEvent(e) {
+            if(!mouseIn){
+                return;
+            }
+            console.log("Mouse move");
+            this.tooltipbox.style.top = e.clientY + 25 + 'px';
+            this.tooltipbox.style.left = e.clientX + 13 +'px';
+        }
+    }
 
-	function createTooltipBox(el) {
-		let tooltip = document.createElement('div');
-		tooltip.innerText = el.getAttribute('data-tooltip-label');
-		tooltip.classList.add('tooltip');
+    function createTooltipBox(el) {
+        let tooltip = document.createElement('div');
+        tooltip.innerText = el.getAttribute('data-tooltip-label');
+        tooltip.classList.add('tooltip');
 
-		document.body.appendChild(tooltip);
+        document.body.appendChild(tooltip);
 
-		return tooltip;
-	}
+        return tooltip;
+    }
 }
 
 initTooltip();
